d009af6b7627ec0e2a989d0347c2e41355d18358
Mrlolpro <leonardoollero14@gmail.com>
2022-08-08 08:50:53 -0300

    xml & text support

--- src/index.ts
+++ src/index.ts
@@ -3,44 +3,62 @@ const url = "https://api.lolinck.xyz/v1/check/";
 
 module.exports = async (
   link: string,
-  options: { real_time: boolean; type: string }
+  options?: { real_time: boolean; type: string }
 ) => {
   var validTypes: Array<String> = ["json", "text", "xml"];
   if (!validTypes.includes(options.type.toLowerCase())) {
     throw new Error("Invalid type");
   }
-  var res: {
-    url: string;
-    NSFW: boolean;
-    MALWARE: boolean;
-    PHISHING: boolean;
-    IP_LOGGING: boolean;
-  } = await got
-    .post(url, {
+  if (options.type == "json" || !options.type) {
+    var res: {
+      url: string;
+      NSFW: boolean;
+      MALWARE: boolean;
+      PHISHING: boolean;
+      IP_LOGGING: boolean;
+    } = await got
+      .post(url, {
+        json: {
+          link: link,
+          real_time: options.real_time || false,
+          type: options.type || "json",
+        },
+      })
+      .json();
+    var safe = false;
+    if (
+      res.NSFW == false &&
+      res.MALWARE == false &&
+      res.PHISHING == false &&
+      res.IP_LOGGING == false
+    ) {
+      safe = true;
+    }
+    var formatRes: object = {
+      url: res.url,
+      safe: safe,
+      nsfw: res.NSFW,
+      malware: res.MALWARE,
+      phising: res.PHISHING,
+      ip_logging: res.IP_LOGGING,
+    };
+
+    return formatRes;
+  } else {
+    var res: {
+      url: string;
+      NSFW: boolean;
+      MALWARE: boolean;
+      PHISHING: boolean;
+      IP_LOGGING: boolean;
+    } = await got.post(url, {
       json: {
         link: link,
         real_time: options.real_time || false,
-        type: options.type || "json",
+        type: options.type || "text",
       },
-    })
-    .json();
-  var safe = false;
-  if (
-    res.NSFW == false &&
-    res.MALWARE == false &&
-    res.PHISHING == false &&
-    res.IP_LOGGING == false
-  ) {
-    safe = true;
-  }
-  var formatRes: object = {
-    url: res.url,
-    safe: safe,
-    nsfw: res.NSFW,
-    malware: res.MALWARE,
-    phising: res.PHISHING,
-    ip_logging: res.IP_LOGGING,
-  };
+    });
 
-  return formatRes;
+    return res;
+  }
 };
