1b9a0cc3344701794bc48abd7f7e917a0d92bcdb
Mrlolpro <leonardoollero14@gmail.com>
2022-08-08 07:17:19 -0300

    typescript

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import got from "got";
+const url = "https://api.lolinck.xyz/v1/check/";
+
+module.exports = async (
+  link: string,
+  options: { real_time: boolean; type: string }
+) => {
+  var validTypes: Array<String> = ["json", "text", "xml"];
+  if (!validTypes.includes(options.type.toLowerCase())) {
+    throw new Error("Invalid type");
+  }
+  var res: {
+    url: string;
+    NSFW: boolean;
+    MALWARE: boolean;
+    PHISHING: boolean;
+    IP_LOGGING: boolean;
+  } = await got
+    .post(url, {
+      json: {
+        link: link,
+        real_time: options.real_time || false,
+        type: options.type || "json",
+      },
+    })
+    .json();
+  var formatRes: object = {
+    url: res.url,
+    nsfw: res.NSFW,
+    malware: res.MALWARE,
+    phising: res.PHISHING,
+    ip_logging: res.IP_LOGGING,
+  };
+
+  return formatRes;
+};
