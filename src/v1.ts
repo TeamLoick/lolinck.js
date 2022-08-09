40c4d574a8bec5ea67a278d1a79f4c6140c0bd08
Mrlolpro <leonardoollero14@gmail.com>
2022-08-09 08:39:53 -0300

    only json

--- src/v1.ts
+++ src/v1.ts
@@ -5,60 +5,42 @@ module.exports = async (
   link: string,
   options?: { real_time: boolean; type: string }
 ) => {
-  var validTypes: Array<String> = ["json", "text", "xml"];
+  var validTypes: Array<String> = ["json"];
   if (!validTypes.includes(options.type.toLowerCase())) {
     throw new Error("Invalid type");
   }
-  if (options.type == "json" || !options.type) {
-    var res: {
-      url: string;
-      NSFW: boolean;
-      MALWARE: boolean;
-      PHISHING: boolean;
-      IP_LOGGING: boolean;
-    } = await got
-      .post(url, {
-        json: {
-          link: link,
-          real_time: options.real_time || false,
-          type: options.type || "json",
-        },
-      })
-      .json();
-    var safe = false;
-    if (
-      res.NSFW == false &&
-      res.MALWARE == false &&
-      res.PHISHING == false &&
-      res.IP_LOGGING == false
-    ) {
-      safe = true;
-    }
-    var formatRes: object = {
-      url: res.url,
-      safe: safe,
-      nsfw: res.NSFW,
-      malware: res.MALWARE,
-      phising: res.PHISHING,
-      ip_logging: res.IP_LOGGING,
-    };
-
-    return formatRes;
-  } else {
-    var res: {
-      url: string;
-      NSFW: boolean;
-      MALWARE: boolean;
-      PHISHING: boolean;
-      IP_LOGGING: boolean;
-    } = await got.post(url, {
+  var res: {
+    url: string;
+    NSFW: boolean;
+    MALWARE: boolean;
+    PHISHING: boolean;
+    IP_LOGGING: boolean;
+  } = await got
+    .post(url, {
       json: {
         link: link,
         real_time: options.real_time || false,
-        type: options.type || "text",
+        type: options.type || "json",
       },
-    });
-
-    return res;
+    })
+    .json();
+  var safe = false;
+  if (
+    res.NSFW == false &&
+    res.MALWARE == false &&
+    res.PHISHING == false &&
+    res.IP_LOGGING == false
+  ) {
+    safe = true;
   }
+  var formatRes: object = {
+    url: res.url,
+    safe: safe,
+    nsfw: res.NSFW,
+    malware: res.MALWARE,
+    phising: res.PHISHING,
+    ip_logging: res.IP_LOGGING,
+  };
+
+  return formatRes;
 };
